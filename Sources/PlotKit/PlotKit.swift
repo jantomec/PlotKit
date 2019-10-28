a9e5e647cbdc83bc9df3d40498ff5d6d088320f6
Jan Tomec <jan.tomec@gmail.com>
2019-10-28 10:15:26 +0100

    init

--- Sources/PlotKit/PlotKit.swift
+++ Sources/PlotKit/PlotKit.swift
@@ -1,3 +1,9 @@
-struct PlotKit {
-    var text = "Hello, World!"
+import CoreGraphics
+
+public func plot<T: Numeric>(x: [T], y: [T]) {
+    
+    let ctx = CGContext(data: nil, width: 300, height: 300, bitsPerComponent: 8, bytesPerRow: 0, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGBitmapInfo.alphaInfoMask.rawValue)
+    
+    
+    
 }
--- Tests/PlotKitTests/PlotKitTests.swift
+++ Tests/PlotKitTests/PlotKitTests.swift
@@ -6,7 +6,7 @@ final class PlotKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PlotKit().text, "Hello, World!")
+        XCTAssert(true)
     }
 
     static var allTests = [
